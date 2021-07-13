a0bcdf404b973c8ba6615aa93ae859b3d330eec3
kukuhujianto <kukuhujianto99@gmail.com>
2021-07-14 01:51:58 +0700

    commit1

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,86 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container" style="margin-top: 20px;">
+    <div class="card">
+      <div class="card-body">
+        <h5 class="card-title">Simple Todo App</h5>
+        <div class="row">
+          <div class="col-10">
+            <!-- @keyup.enter="add" >> jika klik enter akan menjalankan method add -->
+            <input
+              v-model="todo"
+              type="text"
+              class="form-control"
+              @keyup.enter="add"
+            />
+          </div>
+          <div class="col-2">
+            <button class="btn btn-success" @click="add">Add</button>
+          </div>
+        </div>
+        <br />
+        <list :todos="list" @deleteTodo="deleteTodo" @doneTodo="doneTodo" />
+        <small>Total Todo: {{ totalTODO }}</small>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import { ref, reactive, toRefs, onMounted, computed } from "vue";
+import List from "./components/List.vue";
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
+  components: { List },
+  setup() {
+    const todo = ref("");
+    const todos = reactive({
+      list: []
+    });
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+    //onMounted = fungsi yang pertama kali dijalankan
+    onMounted(() => {
+      const items = localStorage.getItem("todos");
+      //if isi items ada maka ? simpan di local storage, jika tidak ada maka : array kosong
+      todos.list = items ? JSON.parse(items) : [];
+    });
+
+    const totalTODO = computed(() => {
+      return todos.list.length;
+    });
+
+    const add = () => {
+      todos.list.unshift({
+        activity: todo.value,
+        isDone: false
+      });
+      todo.value = "";
+      saveToLocalStorage();
+    };
+
+    const deleteTodo = todoIndex => {
+      todos.list = todos.list.filter((item, index) => {
+        if (index != todoIndex) {
+          return item;
+        }
+      });
+      saveToLocalStorage();
+    };
+
+    const doneTodo = todoIndex => {
+      todos.list = todos.list.filter((item, index) => {
+        if (index == todoIndex) {
+          item.isDone = true;
+        }
+        return item;
+      });
+      saveToLocalStorage();
+    };
+
+    const saveToLocalStorage = () => {
+      localStorage.setItem("todos", JSON.stringify(todos.list));
+    };
+
+    return { todo, ...toRefs(todos), totalTODO, add, deleteTodo, doneTodo };
+  }
+};
+</script>
---
+++ src/components/List.vue
@@ -0,0 +1,52 @@
+<template>
+  <ul class="list-group">
+    <li class="list-group-item" v-for="(todo, index) in todos" :key="index">
+      <div class="row">
+        <div class="col-8">
+          <span v-if="todo.isDone">
+            <del>{{ todo.activity }}</del>
+          </span>
+          <span v-else>{{ todo.activity }}</span>
+        </div>
+        <div class="col-2">
+          <button class="btn btn-info" @click="done(index)"><i class="bi bi-check-circle"></i></button>
+        </div>
+        <div class="col-2">
+          <button class="btn btn-danger" @click="deleteTODO(index)"><i class="bi bi-trash"></i></button>
+        </div>
+      </div>
+    </li>
+  </ul>
+</template>
+
+<script>
+export default {
+  props: {
+    todos: {
+      type: Array,
+      default() {
+        return []
+      }
+    }
+  },
+  setup(props, { emit }) {
+    const deleteTODO = index => {
+      emit('deleteTodo', index)
+    };
+
+    const done = index => {
+      emit('doneTodo', index)
+    }
+
+    return { deleteTODO, done}
+  },
+  // methods: {
+  //   deleteTODO(index) {
+  //     this.$emit('deleteTodo', index)
+  //   },
+  //   done(index) {
+  //     this.$emit('doneTodo', index)
+  //   }
+  // }
+}
+</script>
